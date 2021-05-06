986c4d20ba181891fdb45a8628da066414e0996d
Antoine Drouault <antoinedrouault@hotmail.com>
2021-05-06 23:28:06 +0200

    new messages appears in chat-history when submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [typedMessage, setTypedMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const submitNewMessage = (e) => {
+    e.preventDefault();
+    setMessages([...messages, typedMessage]);
+    setTypedMessage("");
+  };
   return (
     <div className="App">
       <header className="App-header">BlaBlaChat</header>
+      <div className="chat-history">
+        {messages.map((message) => (
+          <div>{message}</div>
+        ))}
+      </div>
+      <form onSubmit={submitNewMessage}>
+        <textarea
+          value={typedMessage}
+          onChange={(e) => setTypedMessage(e.target.value)}
+        />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
