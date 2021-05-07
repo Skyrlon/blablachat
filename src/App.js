7322bd5906f3a8c37cc9e2ac4c7e87dd434876ce
Antoine Drouault <antoinedrouault@hotmail.com>
2021-05-07 23:46:41 +0200

    added an emoji picker

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
+
 const App = () => {
   const [typedMessage, setTypedMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [showEmojis, setShowEmojis] = useState(false);
 
   const submitNewMessage = (e) => {
     e.preventDefault();
     setMessages([...messages, typedMessage]);
     setTypedMessage("");
   };
+
+  const addEmoji = (emoji) => {
+    setTypedMessage(typedMessage + emoji.native);
+  };
+
   return (
     <div className="App">
       <header className="App-header">BlaBlaChat</header>
@@ -18,11 +27,15 @@ const App = () => {
           <div>{message}</div>
         ))}
       </div>
-      <form onSubmit={submitNewMessage}>
-        <textarea
-          value={typedMessage}
-          onChange={(e) => setTypedMessage(e.target.value)}
-        />
+      <form className="writing-form" onSubmit={submitNewMessage}>
+        <div className="writing-field">
+          <textarea
+            value={typedMessage}
+            onChange={(e) => setTypedMessage(e.target.value)}
+          />
+          <div onClick={() => setShowEmojis(!showEmojis)}>Emoji</div>
+          {showEmojis && <Picker onSelect={addEmoji} />}
+        </div>
         <input type="submit" value="Send" />
       </form>
     </div>
