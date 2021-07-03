d584551b29bb475a35c35d27939b44433a48c1b8
Antoine Drouault <antoinedrouault@hotmail.com>
2021-07-03 23:46:53 +0200

    Add option to modify message sended

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, createRef, useRef } from "react";
 import "./App.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
 import "emoji-mart/css/emoji-mart.css";
 import { Picker } from "emoji-mart";
 import ContentEditable from "react-contenteditable";
@@ -8,9 +11,14 @@ import ClickAwayListener from "react-click-away-listener";
 
 const App = () => {
   const contentEditable = createRef();
+  const contentEditableEdit = createRef();
   const typedMessage = useRef("");
+  const editedMessage = useRef("");
+
   const [messages, setMessages] = useState([]);
   const [showEmojis, setShowEmojis] = useState(false);
+  const [isEditingMessage, setIsEditingMessage] = useState(false);
+  const [idMessageToEdit, setIdMessageToEdit] = useState(undefined);
 
   const submitNewMessage = (e) => {
     e.preventDefault();
@@ -29,6 +37,7 @@ const App = () => {
         : newMessageDate.getSeconds();
 
     let newMessage = {
+      id: messages.length,
       date: `${newMessageHours}:${newMessageMinutes}:${newMessageSeconds}`,
       text: typedMessage.current,
     };
@@ -41,19 +50,42 @@ const App = () => {
     contentEditable.current.innerHTML = `${contentEditable.current.innerHTML} ${emoji.native}`;
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e, type) => {
+    let value;
     const hyperlinkRegex =
       /(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()[\]{};:'".,<>?«»“”‘’]))?/gi;
     if (hyperlinkRegex.test(e.target.value)) {
-      typedMessage.current = e.target.value.replace(
-        hyperlinkRegex,
-        function (match) {
-          return `<a href="${match}">${match}</a>`;
-        }
-      );
+      value = e.target.value.replace(hyperlinkRegex, function (match) {
+        return `<a href="${match}">${match}</a>`;
+      });
     } else {
-      typedMessage.current = e.target.value;
+      value = e.target.value;
     }
+    if (type === "new") {
+      typedMessage.current = value;
+    } else if (type === "edit") {
+      editedMessage.current = value;
+    }
+  };
+
+  const onEditMessage = (id) => {
+    setIsEditingMessage(true);
+    setIdMessageToEdit(id);
+    editedMessage.current = messages.filter((msg) => msg.id === id)[0].text;
+  };
+
+  const submitEditedMessage = (e, msgToEdit) => {
+    e.preventDefault();
+    let idMsgToEdit = messages.indexOf(msgToEdit);
+    let newMsgArray = messages;
+    newMsgArray.splice(idMsgToEdit, 1, {
+      id: msgToEdit.id,
+      date: msgToEdit.date,
+      text: editedMessage.current,
+    });
+    setMessages([...newMsgArray]);
+    setIsEditingMessage(false);
+    setIdMessageToEdit(undefined);
   };
 
   return (
@@ -61,14 +93,51 @@ const App = () => {
       <header className="App-header">BlaBlaChat</header>
       <div className="chat-history">
         {messages.map((message) => (
-          <div className="message" key={message.date}>
+          <div className="message" key={message.id}>
             <div>{message.date + " : "}</div>
-            <div
-              className="message-text"
-              dangerouslySetInnerHTML={{
-                __html: message.text,
-              }}
-            ></div>
+            {!(isEditingMessage && message.id === idMessageToEdit) && (
+              <div
+                className="message-text"
+                dangerouslySetInnerHTML={{
+                  __html: message.text,
+                }}
+              ></div>
+            )}
+            {!(isEditingMessage && message.id === idMessageToEdit) && (
+              <div className="edit-icon">
+                <FontAwesomeIcon
+                  icon={faEdit}
+                  onClick={() => onEditMessage(message.id)}
+                />
+              </div>
+            )}
+            {isEditingMessage && message.id === idMessageToEdit && (
+              <form
+                className="writing-form"
+                onSubmit={(e) => submitEditedMessage(e, message)}
+              >
+                <div className="writing-field">
+                  <ContentEditable
+                    innerRef={contentEditableEdit}
+                    className="writing-input"
+                    html={editedMessage.current}
+                    disabled={false}
+                    onChange={(e) => handleChange(e, "edit")}
+                  />
+
+                  <div onClick={() => setShowEmojis(!showEmojis)}>Emoji</div>
+
+                  {showEmojis && (
+                    <ClickAwayListener onClickAway={() => setShowEmojis(false)}>
+                      <div>
+                        <Picker onSelect={addEmoji} emojiTooltip={true} />
+                      </div>
+                    </ClickAwayListener>
+                  )}
+                </div>
+                <input type="submit" value="Send" />
+              </form>
+            )}
           </div>
         ))}
       </div>
@@ -79,7 +148,7 @@ const App = () => {
             className="writing-input"
             html={typedMessage.current}
             disabled={false}
-            onChange={handleChange}
+            onChange={(e) => handleChange(e, "new")}
           />
 
           <div onClick={() => setShowEmojis(!showEmojis)}>Emoji</div>
