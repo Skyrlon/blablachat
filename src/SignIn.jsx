3de3619a270bf1459bdf0ad3a45b56fe80587e0d
Antoine Drouault <antoinedrouault@hotmail.com>
2021-07-14 23:38:44 +0200

    Add password characters requirements

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
+import ClearIcon from "@material-ui/icons/Clear";
+import CheckIcon from "@material-ui/icons/Check";
 
 const StyledSignIn = styled.form`
   display: flex;
@@ -10,12 +12,54 @@ const StyledSignIn = styled.form`
 const SignIn = () => {
   const [signIn, setSignIn] = useState(true);
 
+  const [password, setPassword] = useState("");
+
+  const [isPasswordMinChar, setIsPasswordMinChar] = useState(false);
+  const [isPasswordUppercase, setIsPasswordUppercase] = useState(false);
+  const [isPasswordLowercase, setIsPasswordLowercase] = useState(false);
+  const [isPasswordNumber, setIsPasswordNumber] = useState(false);
+  const [isPasswordSpecial, setIsPasswordSpecial] = useState(false);
+
+  const handleOnChangePassword = (e) => {
+    if (!signIn) {
+      let passwordChars = e.target.value.split("");
+      passwordChars.length > 7
+        ? setIsPasswordMinChar(true)
+        : setIsPasswordMinChar(false);
+      passwordChars.some(
+        (element) => isNaN(element) && element === element.toUpperCase()
+      )
+        ? setIsPasswordUppercase(true)
+        : setIsPasswordUppercase(false);
+      passwordChars.some(
+        (element) => isNaN(element) && element === element.toLowerCase()
+      )
+        ? setIsPasswordLowercase(true)
+        : setIsPasswordLowercase(false);
+      passwordChars.some((element) => !isNaN(element) && element !== " ")
+        ? setIsPasswordNumber(true)
+        : setIsPasswordNumber(false);
+      passwordChars.some((element) =>
+        ` !"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`.split("").includes(element)
+      )
+        ? setIsPasswordSpecial(true)
+        : setIsPasswordSpecial(false);
+    }
+    setPassword(e.target.value);
+  };
+
   return (
     <StyledSignIn>
-      <label htmlFor="pseudo">Pseudo : </label>
-      <input type="text" name="pseudo" placeholder="Pseudo" />
+      <label htmlFor="username">User name : </label>
+      <input type="text" name="username" placeholder="User name" />
       <label htmlFor="password">Password : </label>
-      <input type="password" name="password" placeholder="Password" />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleOnChangePassword}
+        value={password}
+      />
 
       {!signIn && (
         <>
@@ -25,6 +69,26 @@ const SignIn = () => {
             name="password-confirm"
             placeholder="Password"
           />
+          <ul>
+            Password must have atleast:
+            <li>
+              {isPasswordMinChar ? <CheckIcon /> : <ClearIcon />} 8 characters
+              minimum
+            </li>
+            <li>
+              {isPasswordUppercase ? <CheckIcon /> : <ClearIcon />} 1 uppercase
+              letter
+            </li>
+            <li>
+              {isPasswordLowercase ? <CheckIcon /> : <ClearIcon />} 1 lowercase
+              letter
+            </li>
+            <li>{isPasswordNumber ? <CheckIcon /> : <ClearIcon />} 1 number</li>
+            <li>
+              {isPasswordSpecial ? <CheckIcon /> : <ClearIcon />} 1 special
+              character
+            </li>
+          </ul>
         </>
       )}
       <input type="submit" />
