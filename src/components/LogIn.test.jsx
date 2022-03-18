6f1c0f4bc4daaf4485b36749f388b82e102f50d2
Antoine Drouault <antoinedrouault@hotmail.com>
2022-03-18 14:20:22 +0100

    Add new tests for LogIn

--- src/components/LogIn.test.jsx
+++ src/components/LogIn.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import "@testing-library/jest-dom";
@@ -36,4 +36,92 @@ it("renders submit button", async () => {
   render(<MockLogIn />);
   const buttonElement = screen.getByRole("button", { name: "Log in" });
   expect(buttonElement).toBeInTheDocument();
+});
+
+it("should be able to type in username input", async () => {
+  render(<MockLogIn />);
+  const usernameInput = screen.getByLabelText("Username");
+  fireEvent.change(usernameInput, { target: { value: "SimpleOne" } });
+  expect(usernameInput.value).toBe("SimpleOne");
+});
+
+it("should be able to type in password input", async () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  fireEvent.change(passwordInput, { target: { value: "Password1@" } });
+  expect(passwordInput.value).toBe("Password1@");
+});
+
+it("renders password input with password type", async () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "password");
+});
+
+it("should change password input type after clicking on visibilty button", () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  const visibiltyButton = screen.getByRole("button", {
+    name: "toggle password visibility",
+  });
+  fireEvent.click(visibiltyButton);
+  expect(passwordInput).toHaveAttribute("type", "text");
+});
+
+it("should toggle back to orignal password input type", () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  const visibiltyButton = screen.getByRole("button", {
+    name: "toggle password visibility",
+  });
+  fireEvent.click(visibiltyButton);
+  fireEvent.click(visibiltyButton);
+  expect(passwordInput).toHaveAttribute("type", "password");
+});
+
+it("should show loading button when submit username", async () => {
+  render(<MockLogIn />);
+  const usernameInput = screen.getByLabelText("Username");
+  fireEvent.submit(usernameInput);
+  const loader = screen.getByRole("progressbar");
+  expect(loader).toBeInTheDocument();
+});
+
+it("should show loading button when submit password", async () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  fireEvent.submit(passwordInput);
+  const loader = screen.getByRole("progressbar");
+  expect(loader).toBeInTheDocument();
+});
+
+it("should show loading button when click on submit button", async () => {
+  render(<MockLogIn />);
+  const buttonElement = screen.getByRole("button", { name: "Log in" });
+  fireEvent.submit(buttonElement);
+  const loader = screen.getByRole("progressbar");
+  expect(loader).toBeInTheDocument();
+});
+
+it("should disable submit button when submit username", () => {
+  render(<MockLogIn />);
+  const usernameInput = screen.getByLabelText("Username");
+  const buttonElement = screen.getByRole("button", { name: "Log in" });
+  fireEvent.submit(usernameInput);
+  expect(buttonElement).toBeDisabled();
+});
+
+it("should disable submit button when submit password", () => {
+  render(<MockLogIn />);
+  const passwordInput = screen.getByLabelText("Password");
+  const buttonElement = screen.getByRole("button", { name: "Log in" });
+  fireEvent.submit(passwordInput);
+  expect(buttonElement).toBeDisabled();
+});
+
+it("should disable submit button when clicked on", () => {
+  render(<MockLogIn />);
+  const buttonElement = screen.getByRole("button", { name: "Log in" });
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toBeDisabled();
 });
