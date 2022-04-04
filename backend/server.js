3f5f87843480cb6f83302cb3580373bb8ed3da7d
Antoine Drouault <antoinedrouault@hotmail.com>
2022-04-04 23:59:43 +0200

    Add connection to mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const colors = require('colors')
 const dotenv = require("dotenv").config()
+const connectDB = require('./config/db')
+
+connectDB()
 
 const port = process.env.BACKEND_PORT || 5000
 
