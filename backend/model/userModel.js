c8cc939b449e7fd99b4f3a59e410a38e2ab903a7
Antoine Drouault <antoinedrouault@hotmail.com>
2022-04-05 21:40:12 +0200

    Add user model

---
+++ backend/model/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const {
+    Schema
+} = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+    },
+    friendsID: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    friendsRequests: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+}, {
+    timestamps: true,
+})
+
+
+module.exports = mongoose.model("User", userSchema)
